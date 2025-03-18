3549a6663b64c9993d359c4571b6876e31c7a087
Shashank Vishwakarma <shashank000s4@gmail.com>
2025-03-18 18:26:15 +0530

    Added: s3 service, audio tracks -> upload, store, delete, fetch apis

---
+++ app/models/track.py
@@ -0,0 +1,30 @@
+from app.extensions.extension import db
+from sqlalchemy.dialects.postgresql import UUID, JSONB
+import enum
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime
+
+class Genre(enum.Enum):
+    electronic = "electronic"
+    lofi = "lofi"
+    synthwave = "synthwave"
+    hiphop = "hiphop"                              
+    rock = "rock"
+    pop = "pop"
+    ambient = "ambient"                            
+    jazz = "jazz"                               
+
+class Track(db.Model):
+    __tablename__ = 'tracks'
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.Text)
+    genre = db.Column(db.Enum(Genre))
+    tags = db.Column(JSONB)
+    starting_price = db.Column(db.Numeric, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    s3_url = db.Column(db.String)
+    artist_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=False) 
