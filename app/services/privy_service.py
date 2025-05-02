2baa115dcf3718f1424709bfa3cdb9994449c588
Shashank Vishwakarma <shashank000s4@gmail.com>
2025-05-02 11:22:43 +0530

    Fix: production config

--- app/services/privy_service.py
+++ app/services/privy_service.py
@@ -4,7 +4,7 @@ import base64
 from uuid import UUID
 from app.extensions.extension import db
 from app.models.wallets import Wallet
-from app import app
+from app import current_app.
 
 class PrivyService:
     def __init__(self):
@@ -35,7 +35,7 @@ class PrivyService:
             "privy-app-id": self.app_id,
             "Authorization": f"Basic {base64.b64encode(f'{self.app_id}:{self.secret_key}'.encode()).decode()}",
             "Content-Type": "application/json",
-            "Origin": "http://localhost:5000" if app.config['ENV'] == 'development' else "https://dreamster-be-364c1455b1f3.herokuapp.com"
+            "Origin": "http://localhost:5000" if current_app.config['ENV'] == 'development' else "https://dreamster-be-364c1455b1f3.herokuapp.com"
         }
         response = requests.post(request_url, json=payload, headers=headers)
         response_data = response.json()
@@ -60,7 +60,7 @@ class PrivyService:
             "privy-app-id": self.app_id,
             "Authorization": f"Basic {base64.b64encode(f'{self.app_id}:{self.secret_key}'.encode()).decode()}",
             "Content-Type": "application/json",
-            "Origin": "http://localhost:5000" if app.config['ENV'] == 'development' else "https://dreamster-be-364c1455b1f3.herokuapp.com"
+            "Origin": "http://localhost:5000" if current_app.config['ENV'] == 'development' else "https://dreamster-be-364c1455b1f3.herokuapp.com"
         }
         response = requests.get(request_url, headers=headers)
         return response.json()
